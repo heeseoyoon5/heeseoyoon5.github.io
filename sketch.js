6e6d28e31539bd2454eb14f5542d7f67c9a5b568
heeseoyoon5 <yoon5@sas.upenn.edu>
2025-12-04 06:45:34 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(550, 550);
+  strokeCap(round);
 }
 
 function draw() {
-  background(220);
+  background(random(0,255),random(0,255),random(0,255),50);
+  translate(width / 2, height / 2);
+
+  var circleResolution = int(map(mouseY, 80, height, 2, 80));
+  var radius = mouseX - width / 2;
+  var angle = TAU / circleResolution;
+
+  strokeWeight(mouseY / 40);
+
+  for (var i = 0; i <= circleResolution; i++) {
+    var x = cos(angle * i) * radius;
+    var y = sin(angle * i) * radius;
+    line(0, 0, x, y);
+  }
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
+
+function mousePressed() {
+  stroke(random(0,255),random(0,255),random(0,255));
 }
